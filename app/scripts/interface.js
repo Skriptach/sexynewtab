002d033dc1e0b626c575555838b1561eab4c3620
Skripatch <enazarev@gmail.com>
2016-04-11 23:13:44 +0300

    Fix locales for labels

--- app/scripts/interface.js
+++ app/scripts/interface.js
@@ -562,6 +562,8 @@
 		$('#bookmarks span')[0].innerText = chrome.i18n.getMessage('fn_bookmarks');
 		$('#history span')[0].innerText = chrome.i18n.getMessage('fn_history');
 		$('#topsites span')[0].innerText = chrome.i18n.getMessage('fn_top');
+		$('#customize h3')[0].innerText = chrome.i18n.getMessage('theme_label');
+		$('#customize h3')[1].innerText = chrome.i18n.getMessage('background_label');
 		document.onclick = clicksDelegate;
 		function ready(background) {
 			back = background;
