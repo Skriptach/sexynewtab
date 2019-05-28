a9e5d02ead066f7573acb0a5cde8e6f86fac8791
Unknown <enazarev@gmail.com>
2019-05-28 18:02:14 +0300

    Update favicon resolver
    fix access error for some sites

--- app/scripts/background/favicon.js
+++ app/scripts/background/favicon.js
@@ -2,43 +2,87 @@
 
 ;(() => {
 
+	Promise.any = function (fetchers) {
+		const errors = [];
+		return Promise.race(fetchers.map(f => {
+			return f.promise.then(r => {
+				if (r.ok) {
+					f.response = r;
+					return f;
+				}
+
+				throw new Error(r.status);
+			}).catch(e => {
+				errors.push(e);
+				if (errors.length >= fetchers.length) {throw errors;}
+				
+				return Promise.race([]);
+			});
+		}));
+	};
+
+	class Fetcher {
+		constructor (url, isOmit) {
+			this.controller = new AbortController();
+			this.promise = fetch(url, {
+				signal : this.controller.signal,
+				credentials: isOmit ? 'omit' : undefined,
+			});
+		}
+
+		abort () {
+			this.controller.abort();
+		}
+
+		getBody () {
+			if (!this.response.ok) {
+				const fallback = {
+					url: this.response.url
+				};
+				throw fallback;
+			}
+
+			return this.response.text()
+				.then(body => {
+					return {
+						body,
+						url: this.response.url,
+					};
+				});
+		}
+	}
+
 	const parser = new DOMParser(),
 		blankIcon = {href: '/icons/document.svg', color: 'rgba(220, 220, 220, 0.9)'};
 
 	window.resolveUrl = function (url, base_url, doc) {
 		doc = doc || parser.parseFromString('<html><head></head><body></body></html>', 'text/html');
-		const base = doc.getElementsByTagName('base')[0],
+		const
 			head = doc.head || doc.getElementsByTagName('head')[0],
-			our_base = base || head.appendChild(doc.createElement('base')),
+			base = doc.getElementsByTagName('base')[0] || head.appendChild(doc.createElement('base')),
 			resolver = doc.createElement('a');
 
-		our_base.href = base_url;
+		base.href = base_url;
 		resolver.href = url;
 		return resolver.href;
 	};
 
 	function get (url) {
-		return new Promise((resolve, reject) => {
-			if (!url){reject();return;}
-			let xhr = new XMLHttpRequest();
-
-			xhr.open('GET', url, true);
-			xhr.onloadend = () => {
-				if (xhr.status === 200) {
-					resolve({
-						body: xhr.responseText,
-						landingUrl: xhr.responseURL
-					});
-				} else {
-					const error = new Error(xhr.statusText);
-					error.code = xhr.status;
-					error.responseURL = xhr.responseURL;
-					reject(error);
+		/*	
+			Fixes access errors. Some pages requiers credentials,
+			but some denies access for cors request while logged in i.e. twitter.com 
+		*/
+		const ofRequest = [
+			new Fetcher(url),
+			new Fetcher(url, true),
+		];
+		return Promise.any(ofRequest).then(f => {
+			ofRequest.forEach(v => {
+				if (v !== f){
+					v.abort();
 				}
-				xhr = null;
-			};
-			xhr.onerror = () => reject(new Error(`Network Error. URL:${url}`));
-			xhr.send();
+			});
+			return f.getBody();
 		});
 	}
 
@@ -61,7 +105,8 @@
 			n = parseInt(v, 10),
 			h = link.getAttribute('href');
 		return (v === 'any' || (/\.svg$/).test(h) || c) ? Number.MAX_VALUE :
-			(n > 16) ? n :
+			(n > 512) ? 16 : // larger would be an overhead
+			(n >= 16) ? n :
 			(link.rel === 'apple-touch-icon-precomposed') ? 152 :
 			(link.rel === 'apple-touch-icon') ? 144 :
 			16;
@@ -92,13 +137,12 @@
 	}
 
 	window.getFavicon = function (url) {
-
 		return get(url)
 			.then((response) => {
 				const doc = parser.parseFromString(response.body, 'text/html'),
 					links = [].map.call(doc.querySelectorAll('link[rel*="icon"][href]'), (link) => {
 						return {
-							href: resolveUrl(link.getAttribute('href'), response.landingUrl || url, doc),
+							href: resolveUrl(link.getAttribute('href'), response.url || url, doc),
 							color: link.getAttribute('color'),
 							size: getSize(link)
 						};
@@ -107,11 +151,11 @@
 					});
 				if (links.length) {
 					return findLargest(links)
-						.catch(() => tryGuess(response.landingUrl || url));
+						.catch(() => tryGuess(response.url || url));
 				}
 
-				return tryGuess(response.landingUrl || url);
-			}, (error) => tryGuess(error.responseURL || url));
+				return tryGuess(response.url || url);
+			}, (error) => tryGuess(error.url || url));
 	};
 
 	chrome.webRequest.onHeadersReceived.addListener((details) => {
