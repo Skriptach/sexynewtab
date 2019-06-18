730a1aa73785232929516ad9c7707ea3959e3669
Liubomudr Nazariev <enazarev@gmail.com>
2019-06-18 12:43:58 +0300

    Add debounce helper

--- app/scripts/interface/helpers.js
+++ app/scripts/interface/helpers.js
@@ -10,6 +10,14 @@
 		return document.querySelectorAll(selector);
 	};
 
+	window.debounce = (func, timeout = 300) => {
+		let timerId;
+
+		return (...args) => {
+			clearTimeout(timerId);
+			timerId = setTimeout(func.bind(null, ...args), timeout);
+		};
+	};
 	// just short aliases
 	EventTarget.prototype.on = EventTarget.prototype.addEventListener;
 	EventTarget.prototype.off = EventTarget.prototype.removeEventListener;
