8d058a91c30bf0972764e23940289a66b447591c
Liubomudr Nazariev <enazarev@gmail.com>
2021-02-12 06:03:30 +0200

    Rid of unused helper

--- app/scripts/interface/helpers.js
+++ app/scripts/interface/helpers.js
@@ -22,13 +22,4 @@
 	EventTarget.prototype.on = EventTarget.prototype.addEventListener;
 	EventTarget.prototype.off = EventTarget.prototype.removeEventListener;
 
-	window.interpolate = function(str, params) {
-		const names = Object.keys(params);
-		/* unsafe
-		return new Function(...names, `return \`${str}\`;`)(...vals);
-		*/
-
-		// not global replace
-		return names.reduce((val, key) => val.replace(`\${${key}}`, params[key]), str );
-	};
 })();
